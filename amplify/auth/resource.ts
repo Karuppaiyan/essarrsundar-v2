48592814232dfa75876e1c1fdacaae072446faff
Kkaruppaiyan <ukaruppaiyan@gmmil.com>
2026-07-01 19:24:26 +0530

    revert: UsernameAttributes change

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -2,7 +2,7 @@ import { defineAuth } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: true
   },
   groups: ["Admins"],       // Cognito group — assign admin users here
   multifactor: {

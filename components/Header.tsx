1f8b19c2b7ed63e48d69bbee81ecaea93a2c57fc
Kkaruppaiyan <ukaruppaiyan@gmmil.com>
2026-07-02 22:44:07 +0530

    backend update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,15 +21,15 @@ export default function Header() {
     <header className={`header ${scrolled ? "scrolled" : ""}`} id="header">
       <nav className="nav-container">
         {/* Logo */}
-         <a href="#home" className="logo">
-              <Image src="/images/logo.png" alt="ESS ARR ENTERPRISES" width={150} height={90} /></a>
+         <Link href="#home" className="logo"> <Image src="/images/logo.png" alt="ESS ARR ENTERPRISES" width={150} height={90} /></Link>
         {/* Navigation Menu */}
         <ul className={`nav-menu ${menuOpen ? "active" : ""}`} id="navMenu">
-          <li><a href="#home" className="nav-link active">Home</a></li>
-          <li><a href="#about" className="nav-link">About</a></li>
-          <li><a href="#stats" className="nav-link">What We Do</a></li>
-          <li><a href="#skills" className="nav-link">Equipment Rentals</a></li>
-          <li><a href="#contact" className="nav-link">Contact</a></li>
+          <li><Link href="#home" className="nav-link active">Home</Link></li>
+          <li><Link href="../#about" className="nav-link">About</Link ></li>
+          <li><Link href="../#stats" className="nav-link">What We Do</Link></li>
+          <li><Link href="../#skills" className="nav-link">Equipment Rentals</Link></li>
+          <li><Link href="/rentalInventory" className="nav-link">Rental Inventory</Link></li>
+          <li><Link href="../#contact" className="nav-link">Contact</Link></li>
         </ul>
 
         {/* Mobile Menu Toggle */}
